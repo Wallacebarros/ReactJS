8fdc3818283c227a00cb79ed5e25e088898deaa5
Wallace Barros da Cruz <104733992+Wallacebarros@users.noreply.github.com>
2024-04-09 15:33:57 +0000

    componets

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,9 +1,14 @@
 import './App.css'
+import Header from './componets/Header/Header'
+import Main from './componets/Main/Main'
+import Footer from './componets/Footer/Footer'
 
 export default function App() {
   return (
     <>
-      <h1>My App</h1>
+      <Header/>
+      <Main/>
+      <Footer/>
     </>
   )
 }
