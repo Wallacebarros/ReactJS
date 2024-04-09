1ab447d4c1dca0325e7b63e8d80a9648072caebe
Wallace Barros da Cruz <104733992+Wallacebarros@users.noreply.github.com>
2024-04-09 18:09:21 +0000

    Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <>
       <Header/>
-      <Main/>
+      <Main exemplo = "props exemplo"/>
       <Footer/>
     </>
   )
--- my-react-app/src/componets/Main/Main.jsx
+++ my-react-app/src/componets/Main/Main.jsx
@@ -1,9 +1,10 @@
 import "./Main.css"
 
-export default function Main() {
+export default function Main({exemplo}) {
     return(
         <main className="Main">
             <p>Main</p>
+            <p>{exemplo}</p>
         </main>
     )
 }
